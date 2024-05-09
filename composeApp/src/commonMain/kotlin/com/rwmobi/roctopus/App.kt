e106117a134fa482514360a6a5c6378c107b386b
Ryan Wong <ryan@rwmobi.com>
2024-05-09 06:54:11 +0100

    [feature] App: temporarily modify the screen to take a list of strings for lazycolumn

--- composeApp/src/commonMain/kotlin/com/rwmobi/roctopus/App.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/roctopus/App.kt
@@ -7,44 +7,68 @@
 
 package com.rwmobi.roctopus
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import co.touchlab.kermit.Logger
+import com.rwmobi.roctopus.ui.theme.AppTheme
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import roctopus.composeapp.generated.resources.Res
-import roctopus.composeapp.generated.resources.compose_multiplatform
+import roctopus.composeapp.generated.resources.app_name
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
-fun App() {
+fun App(strings: List<String> = emptyList()) {
     Logger.d(tag = "tag", messageString = "This is how we log in this project.")
 
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+    AppTheme {
+        Surface {
+//            var showContent by remember { mutableStateOf(false) }
+            Column {
+                Text(text = stringResource(Res.string.app_name))
+//                Button(onClick = { showContent = !showContent }) {
+//                    Text("Click me!")
+//                }
+//                AnimatedVisibility(showContent) {
+//                    val greeting = remember { Greeting().greet() }
+//                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//                        Image(painterResource(Res.drawable.compose_multiplatform), null)
+//                        Text("Compose: $greeting")
+//                    }
+//                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(1f),
+                ) {
+                    items(strings) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            text = it,
+                        )
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                                .fillMaxWidth(),
+                        )
+                    }
                 }
             }
         }
