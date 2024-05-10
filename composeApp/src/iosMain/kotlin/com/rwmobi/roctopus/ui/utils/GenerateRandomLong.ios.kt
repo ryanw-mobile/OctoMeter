2dc95c22f7131bcf7ede63ad03e12923d9e65193
Ryan Wong <ryan@rwmobi.com>
2024-05-10 19:19:01 +0100

    [feature] App: implement cross-platform generateRandomLong

---
+++ composeApp/src/iosMain/kotlin/com/rwmobi/roctopus/ui/utils/GenerateRandomLong.ios.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+package com.rwmobi.roctopus.ui.utils
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UByteVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.get
+import kotlinx.cinterop.memScoped
+import platform.Foundation.NSUUID
+
+
+@OptIn(ExperimentalForeignApi::class)
+actual fun generateRandomLong(): Long {
+    // Proposed by ChatGPT
+    val uuid = NSUUID()
+    return memScoped {
+        val bytes = allocArray<UByteVar>(16) // Allocate memory for 16 unsigned bytes of UUID
+        uuid.getUUIDBytes(bytes) // Fill the array with the UUID bytes
+
+        // Constructing the Long from the first 8 bytes
+        var mostSignificantBits: Long = 0
+        for (i in 0 until 8) {
+            mostSignificantBits = (mostSignificantBits shl 8) or (bytes[i].toInt() and 0xFF).toLong()
+        }
+        mostSignificantBits
+    }
+}
