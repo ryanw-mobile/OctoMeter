ec6babe1d3dd134c28f4e9c72f05affb2ee0bcdb
Ryan Wong <ryan@rwmobi.com>
2024-05-11 20:20:09 +0100

    [feature] App: update custom font configuration

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/theme/Type.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/theme/Type.kt
@@ -3,8 +3,12 @@ package com.rwmobi.kunigami.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import kunigami.composeapp.generated.resources.Res
+import kunigami.composeapp.generated.resources.nunito_italic_variablefont
 import kunigami.composeapp.generated.resources.nunito_variablefont
+import kunigami.composeapp.generated.resources.pathwayextreme_italic_variablefont
 import kunigami.composeapp.generated.resources.pathwayextreme_variablefont
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.Font
@@ -12,8 +16,46 @@ import org.jetbrains.compose.resources.Font
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 internal fun AppTypography(): Typography {
-    val bodyFontFamily = FontFamily(Font(Res.font.pathwayextreme_variablefont))
-    val displayFontFamily = FontFamily(Font(Res.font.nunito_variablefont))
+    val bodyFontFamily = FontFamily(
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W100, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W200, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W300, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W400, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W500, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W600, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W700, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W800, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_variablefont, weight = FontWeight.W900, style = FontStyle.Normal),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W100, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W200, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W300, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W400, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W500, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W600, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W700, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W800, style = FontStyle.Italic),
+        Font(resource = Res.font.pathwayextreme_italic_variablefont, weight = FontWeight.W900, style = FontStyle.Italic),
+    )
+    val displayFontFamily = FontFamily(
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W100, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W200, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W300, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W400, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W500, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W600, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W700, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W800, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_variablefont, weight = FontWeight.W900, style = FontStyle.Normal),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W100, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W200, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W300, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W400, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W500, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W600, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W700, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W800, style = FontStyle.Italic),
+        Font(resource = Res.font.nunito_italic_variablefont, weight = FontWeight.W900, style = FontStyle.Italic),
+    )
 
     // Default Material 3 typography values
     val baseline = Typography()
