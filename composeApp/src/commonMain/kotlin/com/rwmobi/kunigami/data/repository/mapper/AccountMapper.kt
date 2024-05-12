14b21d70b9cb434609a0173855490c7fa62b8d54
Ryan Wong <ryan@rwmobi.com>
2024-05-12 03:16:15 +0100

    [feature] AccountMapper: update to propagate more data to domain model

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/data/repository/mapper/AccountMapper.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/data/repository/mapper/AccountMapper.kt
@@ -7,17 +7,43 @@
 
 package com.rwmobi.kunigami.data.repository.mapper
 
+import com.rwmobi.kunigami.data.source.network.dto.AgreementDto
+import com.rwmobi.kunigami.data.source.network.dto.ElectricityMeterPointDto
 import com.rwmobi.kunigami.data.source.network.dto.PropertyDto
 import com.rwmobi.kunigami.domain.model.Account
+import com.rwmobi.kunigami.domain.model.Agreement
+import com.rwmobi.kunigami.domain.model.ElectricityMeterPoint
 
-fun PropertyDto.toAccount() = Account(
+fun PropertyDto.toAccount(accountNumber: String) = Account(
     id = id,
+    accountNumber = accountNumber,
     movedInAt = movedInAt,
     movedOutAt = movedOutAt,
-    addressLine1 = addressLine1,
-    addressLine2 = addressLine2,
-    addressLine3 = addressLine3,
-    town = town,
-    county = county,
-    postcode = postcode,
+    fullAddress = mergeAddress(
+        addressLine1,
+        addressLine2,
+        addressLine3,
+        town,
+        county,
+        postcode,
+    ),
+    electricityMeterPoints = electricityMeterPoints.map { it.toElectricityMeterPoint() },
 )
+
+fun ElectricityMeterPointDto.toElectricityMeterPoint() = ElectricityMeterPoint(
+    mpan = mpan,
+    meterSerialNumbers = meters.map { it.serialNumber },
+    agreements = agreements.map { it.toAgreement() },
+)
+
+fun AgreementDto.toAgreement() = Agreement(
+    tariffCode = tariffCode,
+    validFrom = validFrom,
+    validTo = validTo,
+)
+
+private fun mergeAddress(vararg addressLines: String?): String? {
+    val filteredAddressLines = addressLines.toList().filterNotNull().filter { it.trim().isNotBlank() }
+    if (filteredAddressLines.isEmpty()) return null
+    return filteredAddressLines.joinToString(separator = "\n")
+}
