6de6aa8ced7538b9747b1d6a8f10578499b9bfc2
Ryan Wong <ryan@rwmobi.com>
2024-05-12 03:16:41 +0100

    [feature] Account Screen: draft UI (wip)

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/account/AccountScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/account/AccountScreen.kt
@@ -9,14 +9,27 @@ package com.rwmobi.kunigami.ui.destinations.account
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.rwmobi.kunigami.ui.theme.AppTheme
+import com.rwmobi.kunigami.ui.theme.getDimension
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 @Composable
 fun AccountScreen(
@@ -34,15 +47,85 @@ fun AccountScreen(
         }
     }
 
+    val dimension = LocalDensity.current.getDimension()
     val scrollState = rememberScrollState()
-    Column(
-        modifier = modifier.verticalScroll(state = scrollState),
-    ) {
-        Text(text = "Account screen")
-        Text("${uiState.account?.fullAddress}")
-        Text("Moved in at: ${uiState.account?.movedInAt}")
-        uiState.account?.movedOutAt?.let {
-            Text("Moved out at: $it")
+
+    if (!uiState.isLoading) {
+        if (uiState.account != null) {
+            Column(
+                modifier = modifier
+                    .verticalScroll(state = scrollState)
+                    .padding(horizontal = dimension.grid_2),
+            ) {
+                with(uiState.account) {
+                    Text(text = "Account number: $accountNumber")
+                    Text("$fullAddress")
+                    movedInAt?.let {
+                        Text("Moved in at: ${it.toLocalDateTime(TimeZone.currentSystemDefault()).date}")
+                    }
+                    movedOutAt?.let {
+                        Text("Moved out at: ${it.toLocalDateTime(TimeZone.currentSystemDefault()).date}")
+                    }
+
+                    Spacer(modifier = Modifier.size(size = dimension.grid_1))
+
+                    Text(
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        text = "Meter information",
+                    )
+
+                    electricityMeterPoints.forEach { meterPoint ->
+                        Text("MPAN: ${meterPoint.mpan}")
+                        Text("Meter serial numbers:")
+                        meterPoint.meterSerialNumbers.forEach {
+                            Text(text = it)
+                        }
+                        Text(
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                            text = "Tariff",
+                        )
+                        meterPoint.agreements.forEach { agreement ->
+                            Text(
+                                style = MaterialTheme.typography.titleMedium,
+                                text = agreement.tariffCode,
+                            )
+                            Text(
+                                text = "From ${agreement.validFrom.toLocalDateTime(TimeZone.currentSystemDefault()).date} to ${agreement.validTo?.toLocalDateTime(TimeZone.currentSystemDefault())?.date}",
+                            )
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.size(size = dimension.grid_3))
+
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {},
+                ) {
+                    Text("Log Out")
+                }
+
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(vertical = dimension.grid_1)
+                        .fillMaxWidth(),
+                )
+
+                Text(
+                    modifier = modifier.padding(vertical = dimension.grid_2),
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                    text = "Version 1.0.0\nAPI provided by Octopus Energy subject to their terms of service.",
+                )
+            }
+        } else {
+            Column(
+                modifier = modifier.verticalScroll(state = scrollState),
+            ) {
+                Text(text = "Account details not found or you don't have an account yet.")
+            }
         }
     }
 
