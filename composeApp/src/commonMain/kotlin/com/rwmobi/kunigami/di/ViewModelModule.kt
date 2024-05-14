0688ef5e9838b24c36c121e95110d2994eaa1545
Ryan Wong <ryan@rwmobi.com>
2024-05-14 23:38:40 +0100

    [chore] App: remove onboarding section

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/di/ViewModelModule.kt
@@ -9,20 +9,12 @@ package com.rwmobi.kunigami.di
 
 import com.rwmobi.kunigami.ui.viewmodels.AccountViewModel
 import com.rwmobi.kunigami.ui.viewmodels.AgileViewModel
-import com.rwmobi.kunigami.ui.viewmodels.OnboardingViewModel
 import com.rwmobi.kunigami.ui.viewmodels.TariffsViewModel
 import com.rwmobi.kunigami.ui.viewmodels.UsageViewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    factory {
-        OnboardingViewModel(
-            octopusRepository = get(),
-            dispatcher = get(named("DefaultDispatcher")),
-        )
-    }
-
     factory {
         UsageViewModel(
             octopusRepository = get(),
