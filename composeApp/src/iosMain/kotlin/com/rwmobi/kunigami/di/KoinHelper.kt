a329410f7adb3277a39f2d5937e854c4d559145d
Ryan Wong <ryan@rwmobi.com>
2024-05-17 12:42:27 +0100

    [chore] App: track DI init time on iOS

--- composeApp/src/iosMain/kotlin/com/rwmobi/kunigami/di/KoinHelper.kt
+++ composeApp/src/iosMain/kotlin/com/rwmobi/kunigami/di/KoinHelper.kt
@@ -6,17 +6,23 @@
  */
 package com.rwmobi.kunigami.di
 
+import co.touchlab.kermit.Logger
 import org.koin.core.context.startKoin
+import kotlin.time.measureTime
 
 fun initKoin() {
-    startKoin {
-        modules(
-            appModule,
-            dispatcherModule,
-            viewModelModule,
-            userCaseModule,
-            ktorModule,
-            repositoryModule,
-        )
-    }
+    val timeTaken = measureTime {
+        startKoin {
+            modules(
+                appModule,
+                dispatcherModule,
+                viewModelModule,
+                userCaseModule,
+                ktorModule,
+                repositoryModule,
+            )
+        }
+    }.inWholeMilliseconds
+
+    Logger.v("initKoin") { "It took $timeTaken milliseconds" }
 }
