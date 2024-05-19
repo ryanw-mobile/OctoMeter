93b5b25939c20efdf33a9c8639c998dab7c5c377
Ryan Wong <ryan@rwmobi.com>
2024-05-19 20:43:49 +0100

    [refactor] ProvideSettings.android.kt: update EncryptedSharedPreferences set up

--- composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/data/source/local/preferences/ProvideSettings.android.kt
+++ composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/data/source/local/preferences/ProvideSettings.android.kt
@@ -9,18 +9,22 @@ package com.rwmobi.kunigami.data.source.local.preferences
 
 import android.content.Context
 import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
+import androidx.security.crypto.MasterKey
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
 
 fun provideSettings(context: Context): Settings {
-    val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
     val sharedPreferences = EncryptedSharedPreferences.create(
-        "encrypted_prefs",
-        masterKeyAlias,
         context,
+        "encrypted_prefs",
+        masterKey,
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
     )
+
     return SharedPreferencesSettings(sharedPreferences)
 }
