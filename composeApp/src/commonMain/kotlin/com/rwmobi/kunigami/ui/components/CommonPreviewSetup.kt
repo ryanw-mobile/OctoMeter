27b948ee1ab001d03863f7f9e04785a70fbf24ea
Ryan Wong <ryan@rwmobi.com>
2024-05-29 18:04:43 +0100

    [chore] CommonPreviewSetup:change to use transparent checker background

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/components/CommonPreviewSetup.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/components/CommonPreviewSetup.kt
@@ -7,7 +7,6 @@
 
 package com.rwmobi.kunigami.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,8 +16,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.rwmobi.kunigami.ui.theme.AppTheme
 import com.rwmobi.kunigami.ui.theme.Dimension
 import com.rwmobi.kunigami.ui.theme.getDimension
@@ -33,7 +35,23 @@ internal fun CommonPreviewSetup(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(color = Color.Black)
+            .drawBehind {
+                val size = 16.dp.toPx() // Size of the checker square
+                val lightColor = Color(0xFFD3D3D3) // Light grey color
+                val darkColor = Color(0xFFA9A9A9) // Dark grey color
+
+                for (i in 0 until (size.toInt() * 2)) {
+                    for (j in 0 until (size.toInt() * 2)) {
+                        val color = if ((i + j) % 2 == 0) lightColor else darkColor
+                        drawRect(
+                            color = color,
+                            topLeft = androidx.compose.ui.geometry.Offset(x = i * size, y = j * size),
+                            size = androidx.compose.ui.geometry.Size(size, size),
+                            style = Fill,
+                        )
+                    }
+                }
+            }
             .verticalScroll(
                 state = rememberScrollState(),
             ),
