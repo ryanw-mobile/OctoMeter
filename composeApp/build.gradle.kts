0e337f666e740f5d522dea6d522a96a2a51abdc5
Ryan Wong <ryan@rwmobi.com>
2024-06-05 23:55:45 +0100

    [chore] Github Actions: try to force the London timezone to trigger BST unit tests

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -313,6 +313,12 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
 tasks.named("preBuild") {
     dependsOn(tasks.named("ktlintFormat"))
 }
+
+tasks.withType<Test> {
+    // Set the timezone to 'Europe/London' for all tests
+    jvmArgs("-Duser.timezone=Europe/London")
+}
+
 dependencies {
     implementation(libs.androidx.profileinstaller)
     "baselineProfile"(project(":baselineprofile"))
