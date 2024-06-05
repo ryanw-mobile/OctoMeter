8057d407fc0866e8152bbc0188f9bb41e5610151
Ryan Wong <ryan@rwmobi.com>
2024-06-05 01:36:26 +0100

    [refactor] AgileScreen: move TitleBar out from LazyColumn

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/agile/AgileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/agile/AgileScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -107,158 +108,159 @@ fun AgileScreen(
                     enabled = uiState.rateGroupedCells.isNotEmpty(),
                     lazyListState = lazyListState,
                 ) { contentModifier ->
-                    LazyColumn(
+                    Column(
                         modifier = contentModifier
                             .fillMaxSize()
                             .conditionalBlur(enabled = uiState.isLoading && uiState.barChartData == null),
-                        contentPadding = PaddingValues(bottom = dimension.grid_4),
-                        state = lazyListState,
                     ) {
-                        stickyHeader(key = "header") {
-                            val subtitle = uiState.agileTariffSummary?.let {
-                                val regionCode = it.getRetailRegion() ?: stringResource(resource = Res.string.unknown)
-                                stringResource(resource = Res.string.agile_product_code_retail_region, it.productCode, regionCode)
-                            }
-                            DualTitleBar(
-                                modifier = Modifier
-                                    .background(color = MaterialTheme.colorScheme.secondary)
-                                    .fillMaxWidth()
-                                    .height(height = dimension.minListItemHeight),
-                                title = uiState.agileTariffSummary?.displayName ?: "",
-                                subtitle = subtitle,
-                            )
+                        val subtitle = uiState.agileTariffSummary?.let {
+                            val regionCode = it.getRetailRegion() ?: stringResource(resource = Res.string.unknown)
+                            stringResource(resource = Res.string.agile_product_code_retail_region, it.productCode, regionCode)
                         }
+                        DualTitleBar(
+                            modifier = Modifier
+                                .background(color = MaterialTheme.colorScheme.secondary)
+                                .fillMaxWidth()
+                                .height(height = dimension.minListItemHeight),
+                            title = uiState.agileTariffSummary?.displayName ?: "",
+                            subtitle = subtitle,
+                        )
 
-                        if (uiState.isDemoMode == true) {
-                            item(key = "demoCta") {
-                                DemoModeCtaAdaptive(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(all = dimension.grid_2),
-                                    description = stringResource(resource = Res.string.agile_demo_introduction),
-                                    ctaButtonLabel = stringResource(resource = Res.string.provide_api_key),
-                                    onCtaButtonClicked = uiEvent.onNavigateToAccountTab,
-                                    useWideLayout = uiState.requestedAdaptiveLayout != WindowWidthSizeClass.Compact,
-                                )
-                            }
-                        }
-
-                        uiState.barChartData?.let { barChartData ->
-                            item(key = "chart") {
-                                BoxWithConstraints(
-                                    modifier = Modifier.padding(top = dimension.grid_1),
-                                ) {
-                                    val constraintModifier = when (uiState.requestedChartLayout) {
-                                        is RequestedChartLayout.Portrait -> {
-                                            Modifier
-                                                .fillMaxWidth()
-                                                .aspectRatio(4 / 3f)
-                                        }
-
-                                        is RequestedChartLayout.LandScape -> {
-                                            Modifier
-                                                .fillMaxWidth()
-                                                .height(uiState.requestedChartLayout.requestedMaxHeight)
-                                        }
-                                    }
-
-                                    VerticalBarChart(
-                                        modifier = constraintModifier.padding(all = dimension.grid_2),
-                                        showToolTipOnClick = uiState.showToolTipOnClick,
-                                        entries = barChartData.verticalBarPlotEntries,
-                                        yAxisRange = uiState.rateRange,
-                                        yAxisTitle = stringResource(resource = Res.string.agile_vat_unit_rate),
-                                        labelGenerator = { index ->
-                                            barChartData.labels[index]
-                                        },
-                                        tooltipGenerator = { index ->
-                                            barChartData.tooltips[index]
-                                        },
-                                        colorPalette = colorPalette,
-                                        backgroundPlot = { graphScope ->
-                                            if (uiState.isOnDifferentTariff() &&
-                                                uiState.userProfile?.tariffSummary != null
-                                            ) {
-                                                graphScope.HorizontalLineAnnotation(
-                                                    location = uiState.userProfile.tariffSummary.vatInclusiveUnitRate,
-                                                    lineStyle = LineStyle(
-                                                        brush = SolidColor(MaterialTheme.colorScheme.error),
-                                                        strokeWidth = dimension.grid_0_5,
-                                                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(4f, 4f), 0f),
-                                                        alpha = 0.5f,
-                                                        colorFilter = null, // No color filter
-                                                        blendMode = DrawScope.DefaultBlendMode,
-                                                    ),
-                                                )
-                                            }
-                                        },
+                        LazyColumn(
+                            contentPadding = PaddingValues(bottom = dimension.grid_4),
+                            state = lazyListState,
+                        ) {
+                            if (uiState.isDemoMode == true) {
+                                item(key = "demoCta") {
+                                    DemoModeCtaAdaptive(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(all = dimension.grid_2),
+                                        description = stringResource(resource = Res.string.agile_demo_introduction),
+                                        ctaButtonLabel = stringResource(resource = Res.string.provide_api_key),
+                                        onCtaButtonClicked = uiEvent.onNavigateToAccountTab,
+                                        useWideLayout = uiState.requestedAdaptiveLayout != WindowWidthSizeClass.Compact,
                                     )
                                 }
                             }
-                        }
 
-                        item(key = "tariffDetails") {
-                            AgileTariffCardAdaptive(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(
-                                        start = dimension.grid_3,
-                                        end = dimension.grid_3,
-                                        top = dimension.grid_1,
-                                    ),
-                                agileTariffSummary = uiState.agileTariffSummary,
-                                differentTariffSummary = uiState.userProfile?.tariffSummary,
-                                colorPalette = colorPalette,
-                                rateRange = uiState.rateRange,
-                                rateGroupedCells = uiState.rateGroupedCells,
-                                requestedAdaptiveLayout = uiState.requestedAdaptiveLayout,
-                            )
-                        }
+                            uiState.barChartData?.let { barChartData ->
+                                item(key = "chart") {
+                                    BoxWithConstraints(
+                                        modifier = Modifier.padding(top = dimension.grid_1),
+                                    ) {
+                                        val constraintModifier = when (uiState.requestedChartLayout) {
+                                            is RequestedChartLayout.Portrait -> {
+                                                Modifier
+                                                    .fillMaxWidth()
+                                                    .aspectRatio(4 / 3f)
+                                            }
 
-                        if (uiState.rateGroupedCells.isNotEmpty()) {
-                            item(key = "headingUnitRateDetails") {
-                                Spacer(modifier = Modifier.height(height = dimension.grid_1))
+                                            is RequestedChartLayout.LandScape -> {
+                                                Modifier
+                                                    .fillMaxWidth()
+                                                    .height(uiState.requestedChartLayout.requestedMaxHeight)
+                                            }
+                                        }
 
-                                LargeTitleWithIcon(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(all = dimension.grid_2),
-                                    icon = painterResource(resource = Res.drawable.revenue),
-                                    label = stringResource(resource = Res.string.agile_unit_rate_details),
-                                )
+                                        VerticalBarChart(
+                                            modifier = constraintModifier.padding(all = dimension.grid_2),
+                                            showToolTipOnClick = uiState.showToolTipOnClick,
+                                            entries = barChartData.verticalBarPlotEntries,
+                                            yAxisRange = uiState.rateRange,
+                                            yAxisTitle = stringResource(resource = Res.string.agile_vat_unit_rate),
+                                            labelGenerator = { index ->
+                                                barChartData.labels[index]
+                                            },
+                                            tooltipGenerator = { index ->
+                                                barChartData.tooltips[index]
+                                            },
+                                            colorPalette = colorPalette,
+                                            backgroundPlot = { graphScope ->
+                                                if (uiState.isOnDifferentTariff() &&
+                                                    uiState.userProfile?.tariffSummary != null
+                                                ) {
+                                                    graphScope.HorizontalLineAnnotation(
+                                                        location = uiState.userProfile.tariffSummary.vatInclusiveUnitRate,
+                                                        lineStyle = LineStyle(
+                                                            brush = SolidColor(MaterialTheme.colorScheme.error),
+                                                            strokeWidth = dimension.grid_0_5,
+                                                            pathEffect = PathEffect.dashPathEffect(floatArrayOf(4f, 4f), 0f),
+                                                            alpha = 0.5f,
+                                                            colorFilter = null, // No color filter
+                                                            blendMode = DrawScope.DefaultBlendMode,
+                                                        ),
+                                                    )
+                                                }
+                                            },
+                                        )
+                                    }
+                                }
                             }
-                        }
 
-                        uiState.rateGroupedCells.forEach { rateGroup ->
-                            item(key = "${rateGroup.title}Title") {
-                                RateGroupTitle(
+                            item(key = "tariffDetails") {
+                                AgileTariffCardAdaptive(
                                     modifier = Modifier
                                         .fillMaxWidth()
                                         .padding(
-                                            vertical = dimension.grid_2,
-                                            horizontal = dimension.grid_4,
+                                            start = dimension.grid_3,
+                                            end = dimension.grid_3,
+                                            top = dimension.grid_1,
                                         ),
-                                    title = rateGroup.title,
-                                )
-                            }
-
-                            // We can do fancier grouping, but for now evenly-distributed is ok
-                            val partitionedItems = rateGroup.rates.partitionList(columns = uiState.requestedRateColumns)
-                            val maxRows = partitionedItems.maxOf { it.size }
-
-                            items(maxRows) { rowIndex ->
-                                RateGroupCells(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(
-                                            horizontal = dimension.grid_4,
-                                            vertical = dimension.grid_0_25,
-                                        ),
-                                    partitionedItems = partitionedItems,
-                                    maxInRange = uiState.rateRange.endInclusive,
-                                    rowIndex = rowIndex,
+                                    agileTariffSummary = uiState.agileTariffSummary,
+                                    differentTariffSummary = uiState.userProfile?.tariffSummary,
                                     colorPalette = colorPalette,
+                                    rateRange = uiState.rateRange,
+                                    rateGroupedCells = uiState.rateGroupedCells,
+                                    requestedAdaptiveLayout = uiState.requestedAdaptiveLayout,
                                 )
+                            }
+
+                            if (uiState.rateGroupedCells.isNotEmpty()) {
+                                item(key = "headingUnitRateDetails") {
+                                    Spacer(modifier = Modifier.height(height = dimension.grid_1))
+
+                                    LargeTitleWithIcon(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(all = dimension.grid_2),
+                                        icon = painterResource(resource = Res.drawable.revenue),
+                                        label = stringResource(resource = Res.string.agile_unit_rate_details),
+                                    )
+                                }
+                            }
+
+                            uiState.rateGroupedCells.forEach { rateGroup ->
+                                item(key = "${rateGroup.title}Title") {
+                                    RateGroupTitle(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(
+                                                vertical = dimension.grid_2,
+                                                horizontal = dimension.grid_4,
+                                            ),
+                                        title = rateGroup.title,
+                                    )
+                                }
+
+                                // We can do fancier grouping, but for now evenly-distributed is ok
+                                val partitionedItems = rateGroup.rates.partitionList(columns = uiState.requestedRateColumns)
+                                val maxRows = partitionedItems.maxOf { it.size }
+
+                                items(maxRows) { rowIndex ->
+                                    RateGroupCells(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(
+                                                horizontal = dimension.grid_4,
+                                                vertical = dimension.grid_0_25,
+                                            ),
+                                        partitionedItems = partitionedItems,
+                                        maxInRange = uiState.rateRange.endInclusive,
+                                        rowIndex = rowIndex,
+                                        colorPalette = colorPalette,
+                                    )
+                                }
                             }
                         }
                     }
