c4596edbd31d7aea48ede51fac0a3df879f4db4f
Ryan Wong <ryan@rwmobi.com>
2024-06-09 04:38:48 +0100

    [fix] ProvideSettings.android: add error handling to clear preference if we cannot decrypt existing preference store

--- composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/data/source/local/preferences/ProvideSettings.android.kt
+++ composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/data/source/local/preferences/ProvideSettings.android.kt
@@ -12,19 +12,36 @@ import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
+import javax.crypto.AEADBadTagException
 
 fun provideSettings(context: Context): Settings {
     val masterKey = MasterKey.Builder(context)
         .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
         .build()
 
-    val sharedPreferences = EncryptedSharedPreferences.create(
-        context,
-        "encrypted_prefs",
-        masterKey,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
-    )
+    val sharedPreferencesName = "encrypted_prefs"
 
-    return SharedPreferencesSettings(sharedPreferences)
+    try {
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            sharedPreferencesName,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+        )
+        return SharedPreferencesSettings(sharedPreferences)
+    } catch (ex: AEADBadTagException) {
+        // Delete the corrupted encrypted shared preferences
+        context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE).edit().clear().apply()
+
+        // Retry creating EncryptedSharedPreferences
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            sharedPreferencesName,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+        )
+        return SharedPreferencesSettings(sharedPreferences)
+    }
 }
