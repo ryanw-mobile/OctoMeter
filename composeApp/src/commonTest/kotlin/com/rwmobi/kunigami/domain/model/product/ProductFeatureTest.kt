9fbb2cb42e8af83fa431a171ff991fbed8b8cb5e
Ryan Wong <ryan@rwmobi.com>
2024-06-15 20:10:05 +0100

    [test] ProductReature: add unit tests

---
+++ composeApp/src/commonTest/kotlin/com/rwmobi/kunigami/domain/model/product/ProductFeatureTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+package com.rwmobi.kunigami.domain.model.product
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class ProductFeatureTest {
+
+    @Test
+    fun `fromApiValue should return VARIABLE for variable`() {
+        val feature = ProductFeature.fromApiValue("variable")
+        assertEquals(ProductFeature.VARIABLE, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return GREEN for green`() {
+        val feature = ProductFeature.fromApiValue("green")
+        assertEquals(ProductFeature.GREEN, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return TRACKER for tracker`() {
+        val feature = ProductFeature.fromApiValue("tracker")
+        assertEquals(ProductFeature.TRACKER, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return PREPAY for prepay`() {
+        val feature = ProductFeature.fromApiValue("prepay")
+        assertEquals(ProductFeature.PREPAY, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return BUSINESS for business`() {
+        val feature = ProductFeature.fromApiValue("business")
+        assertEquals(ProductFeature.BUSINESS, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return RESTRICTED for restricted`() {
+        val feature = ProductFeature.fromApiValue("restricted")
+        assertEquals(ProductFeature.RESTRICTED, feature)
+    }
+
+    @Test
+    fun `fromApiValue should return null for unknown value`() {
+        val feature = ProductFeature.fromApiValue("some_unknown_value")
+        assertNull(feature)
+    }
+
+    @Test
+    fun `fromApiValue should return null for null value`() {
+        val feature = ProductFeature.fromApiValue(null)
+        assertNull(feature)
+    }
+
+    @Test
+    fun `fromApiValue should return null for empty string`() {
+        val feature = ProductFeature.fromApiValue("")
+        assertNull(feature)
+    }
+
+    @Test
+    fun `fromApiValue should return correct feature for case insensitive match`() {
+        val featureVariable = ProductFeature.fromApiValue("VaRiAbLe")
+        assertEquals(ProductFeature.VARIABLE, featureVariable)
+
+        val featureGreen = ProductFeature.fromApiValue("GrEeN")
+        assertEquals(ProductFeature.GREEN, featureGreen)
+    }
+}
