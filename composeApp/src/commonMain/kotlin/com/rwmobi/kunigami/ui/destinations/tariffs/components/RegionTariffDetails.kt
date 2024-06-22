1c67c7334937a65bb30b46838e78c15f591cbe5b
Ryan Wong <ryan@rwmobi.com>
2024-06-22 21:26:34 +0100

    [feature] TariffScreen: detail screen now shows retail region name and rates effective date

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/tariffs/components/RegionTariffDetails.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/tariffs/components/RegionTariffDetails.kt
@@ -10,16 +10,19 @@ package com.rwmobi.kunigami.ui.destinations.tariffs.components
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import com.rwmobi.kunigami.domain.extensions.getLocalDateString
 import com.rwmobi.kunigami.domain.extensions.roundToTwoDecimalPlaces
 import com.rwmobi.kunigami.domain.model.product.ExitFeesType
 import com.rwmobi.kunigami.domain.model.product.Tariff
@@ -32,12 +35,14 @@ import kunigami.composeapp.generated.resources.Res
 import kunigami.composeapp.generated.resources.day_unit_rate
 import kunigami.composeapp.generated.resources.night_unit_rate
 import kunigami.composeapp.generated.resources.no
+import kunigami.composeapp.generated.resources.retail_region_unknown
 import kunigami.composeapp.generated.resources.standard_unit_rate
 import kunigami.composeapp.generated.resources.standing_charge
 import kunigami.composeapp.generated.resources.tariffs_direct_debit_monthly
 import kunigami.composeapp.generated.resources.tariffs_dual_fuel_discount
 import kunigami.composeapp.generated.resources.tariffs_exit_fees
 import kunigami.composeapp.generated.resources.tariffs_online_discount
+import kunigami.composeapp.generated.resources.tariffs_tariff_effective
 import kunigami.composeapp.generated.resources.unit_p_day
 import kunigami.composeapp.generated.resources.unit_p_kwh
 import kunigami.composeapp.generated.resources.unit_pound
@@ -56,13 +61,25 @@ internal fun RegionTariffDetails(
         verticalArrangement = Arrangement.spacedBy(space = dimension.grid_2),
     ) {
         with(tariff) {
-            Text(
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.secondary,
-                text = tariffCode,
-            )
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                val region = stringResource(resource = getRetailRegion()?.stringResource ?: Res.string.retail_region_unknown)
+                Text(
+                    modifier = Modifier.weight(1f),
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.secondary,
+                    text = region,
+                )
+
+                Text(
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.Bold,
+                    text = tariffCode,
+                )
+            }
 
             if (tariffPaymentTerm == TariffPaymentTerm.DIRECT_DEBIT_MONTHLY) {
                 Text(
@@ -72,16 +89,6 @@ internal fun RegionTariffDetails(
                     text = stringResource(resource = Res.string.tariffs_direct_debit_monthly),
                 )
             }
-
-            LabelValueRow(
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.bodyMedium,
-                label = stringResource(resource = Res.string.standing_charge),
-                value = stringResource(
-                    resource = Res.string.unit_p_day,
-                    vatInclusiveStandingCharge.roundToTwoDecimalPlaces(),
-                ),
-            )
 
             if (vatInclusiveOnlineDiscount > 0) {
                 LabelValueRow(
@@ -119,6 +126,26 @@ internal fun RegionTariffDetails(
                 } else {
                     stringResource(resource = Res.string.no)
                 },
+            )
+
+            LabelValueRow(
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.bodyMedium,
+                label = stringResource(resource = Res.string.standing_charge),
+                value = stringResource(
+                    resource = Res.string.unit_p_day,
+                    vatInclusiveStandingCharge.roundToTwoDecimalPlaces(),
+                ),
+            )
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.titleMedium,
+                fontStyle = FontStyle.Italic,
+                text = stringResource(
+                    resource = Res.string.tariffs_tariff_effective,
+                    tariff.tariffActiveAt.getLocalDateString().substringBefore(delimiter = ","),
+                ),
             )
 
             vatInclusiveStandardUnitRate?.let { unitRate ->
