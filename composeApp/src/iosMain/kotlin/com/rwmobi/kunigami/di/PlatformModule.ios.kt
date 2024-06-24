d7ac94ab9c816f05b14713beb884ee69d78e4e45
Ryan Wong <ryan@rwmobi.com>
2024-06-24 02:09:01 +0100

    [fix] RoomDB: fix configurations to run on Android and iOS

--- composeApp/src/iosMain/kotlin/com/rwmobi/kunigami/di/PlatformModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/rwmobi/kunigami/di/PlatformModule.ios.kt
@@ -16,15 +16,27 @@ import com.rwmobi.kunigami.data.source.local.preferences.provideSettings
 import composeapp.kunigami.BuildConfig
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.dsl.module
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
 import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 val platformModule = module {
     single<HttpClientEngine> { Darwin.create() }
     single<Settings> { provideSettings(serviceName = BuildConfig.PACKAGE_NAME) }
 
     single<RoomDatabase.Builder<OctometerDatabase>> {
-        val dbFilePath = NSHomeDirectory() + "/octometer_database.db"
+        val documentsDirectory = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = true,
+            error = null,
+        )?.path ?: NSHomeDirectory()
+        val dbFilePath = "$documentsDirectory/octometer_database.db"
         Room.databaseBuilder<OctometerDatabase>(
             name = dbFilePath,
             factory = { OctometerDatabase::class.instantiateImpl() },
