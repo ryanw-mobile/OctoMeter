bde914b24b5c23abdc0b26d7ec96a60f445c5255
Ryan Wong <ryan@rwmobi.com>
2024-06-27 21:23:28 +0100

    [feature] RateMapper: implement RateEntity mappers

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/data/repository/mapper/RateMapper.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/data/repository/mapper/RateMapper.kt
@@ -7,6 +7,8 @@
 
 package com.rwmobi.kunigami.data.repository.mapper
 
+import com.rwmobi.kunigami.data.source.local.database.entity.RateEntity
+import com.rwmobi.kunigami.data.source.local.database.model.RateType
 import com.rwmobi.kunigami.data.source.network.dto.prices.RateDto
 import com.rwmobi.kunigami.domain.model.rate.PaymentMethod
 import com.rwmobi.kunigami.domain.model.rate.Rate
@@ -16,4 +18,22 @@ fun RateDto.toRate() = Rate(
     vatInclusivePrice = vatInclusivePrice,
     validity = validFrom..(validTo ?: Instant.DISTANT_FUTURE),
     paymentMethod = PaymentMethod.fromValue(paymentMethod),
+)
+
+fun RateDto.toRateEntity(
+    tariffCode: String,
+    rateType: RateType,
+) = RateEntity(
+    tariffCode = tariffCode,
+    rateType = rateType,
+    paymentMethod = PaymentMethod.fromValue(value = paymentMethod),
+    validFrom = validFrom,
+    validTo = if (validTo == Instant.DISTANT_FUTURE) null else validTo,
+    vatRate = vatInclusivePrice,
+)
+
+fun RateEntity.toRate() = Rate(
+    vatInclusivePrice = vatRate,
+    validity = validFrom..(validTo ?: Instant.DISTANT_FUTURE),
+    paymentMethod = paymentMethod,
 )
