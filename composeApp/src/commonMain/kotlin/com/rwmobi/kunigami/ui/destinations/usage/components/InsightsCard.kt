4c119e37e8de943a9ef735554fc00ea81fb786cb
Ryan Wong <ryan@rwmobi.com>
2024-06-28 19:01:23 +0100

    [refactor] InsightsCard: apply WidgetCard layout

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/usage/components/InsightsCard.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/usage/components/InsightsCard.kt
@@ -9,27 +9,24 @@ package com.rwmobi.kunigami.ui.destinations.usage.components
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.rwmobi.kunigami.ui.components.CommonPreviewSetup
+import com.rwmobi.kunigami.ui.components.WidgetCard
 import com.rwmobi.kunigami.ui.model.consumption.Insights
 import com.rwmobi.kunigami.ui.previewsampledata.InsightsSamples
 import com.rwmobi.kunigami.ui.theme.getDimension
@@ -52,28 +49,15 @@ internal fun InsightsCard(
 ) {
     val dimension = LocalDensity.current.getDimension()
 
-    Card(modifier = modifier) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(all = dimension.grid_2),
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.labelMedium,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                textAlign = TextAlign.Center,
-                text = pluralStringResource(
-                    resource = Res.plurals.usage_insights_consumption,
-                    quantity = insights.consumptionTimeSpan,
-                    insights.consumptionAggregateRounded,
-                    insights.consumptionTimeSpan,
-                ),
-            )
-
-            Spacer(modifier = Modifier.weight(weight = 1f))
-
+    WidgetCard(
+        modifier = modifier,
+        heading = pluralStringResource(
+            resource = Res.plurals.usage_insights_consumption,
+            quantity = insights.consumptionTimeSpan,
+            insights.consumptionAggregateRounded,
+            insights.consumptionTimeSpan,
+        ),
+        contents = {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center,
@@ -101,8 +85,6 @@ internal fun InsightsCard(
                 )
             }
 
-            Spacer(modifier = Modifier.weight(weight = 1f))
-
             if (insights.consumptionTimeSpan > 1) {
                 Text(
                     modifier = Modifier.fillMaxWidth(),
@@ -116,9 +98,8 @@ internal fun InsightsCard(
                     ),
                 )
             }
-
-            Spacer(modifier = Modifier.weight(weight = 1f))
-
+        },
+        footer = {
             AnimatedRatioBar(
                 modifier = Modifier
                     .padding(top = dimension.grid_1)
@@ -141,8 +122,8 @@ internal fun InsightsCard(
                     text = stringResource(resource = Res.string.standing_charge),
                 )
             }
-        }
-    }
+        },
+    )
 }
 
 @Preview
