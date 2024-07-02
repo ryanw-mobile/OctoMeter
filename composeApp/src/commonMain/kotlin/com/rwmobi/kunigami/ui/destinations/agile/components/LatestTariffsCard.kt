1457494c38b877dadda082eb5dda9523ce344ff0
Ryan Wong <ryan@rwmobi.com>
2024-07-02 21:09:45 +0100

    [feature] LatestTariffsCard: Simplify text and apply FlowRow with 2 columns on wider layouts.

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/agile/components/LatestTariffsCard.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/destinations/agile/components/LatestTariffsCard.kt
@@ -8,9 +8,13 @@
 package com.rwmobi.kunigami.ui.destinations.agile.components
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -30,9 +34,10 @@ import com.rwmobi.kunigami.ui.theme.cyanish
 import com.rwmobi.kunigami.ui.theme.getDimension
 import com.rwmobi.kunigami.ui.theme.purpleish
 import kunigami.composeapp.generated.resources.Res
-import kunigami.composeapp.generated.resources.agile_tariff_standard_unit_rate
+import kunigami.composeapp.generated.resources.unit_p_kwh
 import org.jetbrains.compose.resources.stringResource
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 internal fun LatestTariffsCard(
     modifier: Modifier = Modifier,
@@ -41,26 +46,40 @@ internal fun LatestTariffsCard(
     latestFixedTariffColor: Color,
     latestFlexibleTariffColor: Color,
 ) {
+    val dimension = LocalDensity.current.getDimension()
+
     WidgetCard(
         modifier = modifier,
         contents = {
-            latestFlexibleTariff?.let { tariff ->
-                tariff.vatInclusiveStandardUnitRate?.let { unitRate ->
-                    TariffEntry(
-                        tariff = tariff,
-                        unitRate = unitRate,
-                        indicatorColor = latestFlexibleTariffColor,
-                    )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(space = dimension.grid_1),
+                verticalArrangement = Arrangement.spacedBy(space = dimension.grid_1),
+            ) {
+                latestFlexibleTariff?.let { tariff ->
+                    tariff.vatInclusiveStandardUnitRate?.let { unitRate ->
+                        TariffEntry(
+                            modifier = Modifier
+                                .widthIn(min = dimension.windowWidthCompactOneThird)
+                                .weight(1f),
+                            tariff = tariff,
+                            unitRate = unitRate,
+                            indicatorColor = latestFlexibleTariffColor,
+                        )
+                    }
                 }
-            }
 
-            latestFixedTariff?.let { tariff ->
-                tariff.vatInclusiveStandardUnitRate?.let { unitRate ->
-                    TariffEntry(
-                        tariff = tariff,
-                        unitRate = unitRate,
-                        indicatorColor = latestFixedTariffColor,
-                    )
+                latestFixedTariff?.let { tariff ->
+                    tariff.vatInclusiveStandardUnitRate?.let { unitRate ->
+                        TariffEntry(
+                            modifier = Modifier
+                                .widthIn(min = dimension.windowWidthCompactOneThird)
+                                .weight(1f),
+                            tariff = tariff,
+                            unitRate = unitRate,
+                            indicatorColor = latestFixedTariffColor,
+                        )
+                    }
                 }
             }
         },
@@ -69,13 +88,15 @@ internal fun LatestTariffsCard(
 
 @Composable
 private fun TariffEntry(
+    modifier: Modifier = Modifier,
     tariff: Tariff,
     unitRate: Double,
     indicatorColor: Color,
 ) {
     val dimension = LocalDensity.current.getDimension()
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
             .drawBehind {
                 val width = dimension.grid_2.toPx()
                 drawRect(
@@ -98,7 +119,7 @@ private fun TariffEntry(
             modifier = Modifier.fillMaxWidth(),
             style = MaterialTheme.typography.bodySmall,
             text = stringResource(
-                resource = Res.string.agile_tariff_standard_unit_rate,
+                resource = Res.string.unit_p_kwh,
                 unitRate.roundToTwoDecimalPlaces().toString(),
             ),
         )
