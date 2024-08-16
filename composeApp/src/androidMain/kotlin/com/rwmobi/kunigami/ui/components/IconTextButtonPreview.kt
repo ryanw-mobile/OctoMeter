dd34dfc162500535f3234d178d8c34f7d4ad6ed2
Ryan W <ryan@rwmobi.com>
2024-08-16 01:03:45 +0100

    [test] OctopusUserPreferencesRepository: add unit tests

--- composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/ui/components/IconTextButtonPreview.kt
+++ composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/ui/components/IconTextButtonPreview.kt
@@ -12,17 +12,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewDynamicColors
 import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
-import com.rwmobi.kunigami.R
 import com.rwmobi.kunigami.ui.theme.AppTheme
 import kunigami.composeapp.generated.resources.Res
 import kunigami.composeapp.generated.resources.coin
@@ -34,7 +29,8 @@ private fun Preview() {
     AppTheme {
         Column(
             modifier = Modifier.padding(all = 8.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
             IconTextButton(
                 icon = org.jetbrains.compose.resources.painterResource(resource = Res.drawable.coin),
                 text = "Money Generator",
