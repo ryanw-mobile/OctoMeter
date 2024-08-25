f2b55c47864848dc6b9eaf4053c6a593de1b9606
Ryan W <ryan@rwmobi.com>
2024-08-25 01:20:29 +0100

    [test] UsageViewModel: add unit tests

---
+++ composeApp/src/commonTest/kotlin/com/rwmobi/kunigami/test/samples/ConsumptionSampleData.kt
@@ -0,0 +1,62 @@
+import com.rwmobi.kunigami.domain.model.consumption.Consumption
+import kotlinx.datetime.Instant
+
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+object ConsumptionSampleData {
+    val randomSample = listOf(
+        Consumption(kWhConsumed = 0.165235958596004, interval = Instant.parse("2024-08-22T23:00:00Z")..Instant.parse("2024-08-22T23:30:00Z")),
+        Consumption(kWhConsumed = 0.09940452763221952, interval = Instant.parse("2024-08-22T23:30:00Z")..Instant.parse("2024-08-23T00:00:00Z")),
+        Consumption(kWhConsumed = 0.44884703198248777, interval = Instant.parse("2024-08-23T00:00:00Z")..Instant.parse("2024-08-23T00:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T00:30:00Z")..Instant.parse("2024-08-23T01:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T01:00:00Z")..Instant.parse("2024-08-23T01:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T01:30:00Z")..Instant.parse("2024-08-23T02:00:00Z")),
+        Consumption(kWhConsumed = 0.28573133929126837, interval = Instant.parse("2024-08-23T02:00:00Z")..Instant.parse("2024-08-23T02:30:00Z")),
+        Consumption(kWhConsumed = 0.5097595736386005, interval = Instant.parse("2024-08-23T02:30:00Z")..Instant.parse("2024-08-23T03:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T03:00:00Z")..Instant.parse("2024-08-23T03:30:00Z")),
+        Consumption(kWhConsumed = 0.3033163991134752, interval = Instant.parse("2024-08-23T03:30:00Z")..Instant.parse("2024-08-23T04:00:00Z")),
+        Consumption(kWhConsumed = 0.185290486549797, interval = Instant.parse("2024-08-23T04:00:00Z")..Instant.parse("2024-08-23T04:30:00Z")),
+        Consumption(kWhConsumed = 0.5469903088481638, interval = Instant.parse("2024-08-23T04:30:00Z")..Instant.parse("2024-08-23T05:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T05:00:00Z")..Instant.parse("2024-08-23T05:30:00Z")),
+        Consumption(kWhConsumed = 0.15303057480613907, interval = Instant.parse("2024-08-23T05:30:00Z")..Instant.parse("2024-08-23T06:00:00Z")),
+        Consumption(kWhConsumed = 0.5572410421395602, interval = Instant.parse("2024-08-23T06:00:00Z")..Instant.parse("2024-08-23T06:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T06:30:00Z")..Instant.parse("2024-08-23T07:00:00Z")),
+        Consumption(kWhConsumed = 0.07875303147828325, interval = Instant.parse("2024-08-23T07:00:00Z")..Instant.parse("2024-08-23T07:30:00Z")),
+        Consumption(kWhConsumed = 0.17521369917222113, interval = Instant.parse("2024-08-23T07:30:00Z")..Instant.parse("2024-08-23T08:00:00Z")),
+        Consumption(kWhConsumed = 0.41243018799704745, interval = Instant.parse("2024-08-23T08:00:00Z")..Instant.parse("2024-08-23T08:30:00Z")),
+        Consumption(kWhConsumed = 0.407120707203384, interval = Instant.parse("2024-08-23T08:30:00Z")..Instant.parse("2024-08-23T09:00:00Z")),
+        Consumption(kWhConsumed = 0.3084046466838003, interval = Instant.parse("2024-08-23T09:00:00Z")..Instant.parse("2024-08-23T09:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T09:30:00Z")..Instant.parse("2024-08-23T10:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T10:00:00Z")..Instant.parse("2024-08-23T10:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T10:30:00Z")..Instant.parse("2024-08-23T11:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T11:00:00Z")..Instant.parse("2024-08-23T11:30:00Z")),
+        Consumption(kWhConsumed = 0.24064461934675496, interval = Instant.parse("2024-08-23T11:30:00Z")..Instant.parse("2024-08-23T12:00:00Z")),
+        Consumption(kWhConsumed = 0.08438257327746373, interval = Instant.parse("2024-08-23T12:00:00Z")..Instant.parse("2024-08-23T12:30:00Z")),
+        Consumption(kWhConsumed = 0.19205673611093207, interval = Instant.parse("2024-08-23T12:30:00Z")..Instant.parse("2024-08-23T13:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T13:00:00Z")..Instant.parse("2024-08-23T13:30:00Z")),
+        Consumption(kWhConsumed = 0.3250650615004777, interval = Instant.parse("2024-08-23T13:30:00Z")..Instant.parse("2024-08-23T14:00:00Z")),
+        Consumption(kWhConsumed = 0.45494789287668, interval = Instant.parse("2024-08-23T14:00:00Z")..Instant.parse("2024-08-23T14:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T14:30:00Z")..Instant.parse("2024-08-23T15:00:00Z")),
+        Consumption(kWhConsumed = 0.1980893484281169, interval = Instant.parse("2024-08-23T15:00:00Z")..Instant.parse("2024-08-23T15:30:00Z")),
+        Consumption(kWhConsumed = 0.2229933747408947, interval = Instant.parse("2024-08-23T15:30:00Z")..Instant.parse("2024-08-23T16:00:00Z")),
+        Consumption(kWhConsumed = 0.17072542494030654, interval = Instant.parse("2024-08-23T16:00:00Z")..Instant.parse("2024-08-23T16:30:00Z")),
+        Consumption(kWhConsumed = 0.12325363026046059, interval = Instant.parse("2024-08-23T16:30:00Z")..Instant.parse("2024-08-23T17:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T17:00:00Z")..Instant.parse("2024-08-23T17:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T17:30:00Z")..Instant.parse("2024-08-23T18:00:00Z")),
+        Consumption(kWhConsumed = 0.1501275625524833, interval = Instant.parse("2024-08-23T18:00:00Z")..Instant.parse("2024-08-23T18:30:00Z")),
+        Consumption(kWhConsumed = 0.3045590657185424, interval = Instant.parse("2024-08-23T18:30:00Z")..Instant.parse("2024-08-23T19:00:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T19:00:00Z")..Instant.parse("2024-08-23T19:30:00Z")),
+        Consumption(kWhConsumed = 0.1510989296101421, interval = Instant.parse("2024-08-23T19:30:00Z")..Instant.parse("2024-08-23T20:00:00Z")),
+        Consumption(kWhConsumed = 0.15901166048896162, interval = Instant.parse("2024-08-23T20:00:00Z")..Instant.parse("2024-08-23T20:30:00Z")),
+        Consumption(kWhConsumed = 0.31650428825396915, interval = Instant.parse("2024-08-23T20:30:00Z")..Instant.parse("2024-08-23T21:00:00Z")),
+        Consumption(kWhConsumed = 0.2239049103605202, interval = Instant.parse("2024-08-23T21:00:00Z")..Instant.parse("2024-08-23T21:30:00Z")),
+        Consumption(kWhConsumed = 0.05, interval = Instant.parse("2024-08-23T21:30:00Z")..Instant.parse("2024-08-23T22:00:00Z")),
+        Consumption(kWhConsumed = 0.3622933497727502, interval = Instant.parse("2024-08-23T22:00:00Z")..Instant.parse("2024-08-23T22:30:00Z")),
+        Consumption(kWhConsumed = 0.2349476006387274, interval = Instant.parse("2024-08-23T22:30:00Z")..Instant.parse("2024-08-23T23:00:00Z")),
+    )
+}
---
+++ composeApp/src/commonTest/kotlin/com/rwmobi/kunigami/ui/viewmodels/UsageViewModelTest.kt
@@ -0,0 +1,351 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+package com.rwmobi.kunigami.ui.viewmodels
+
+import ConsumptionSampleData
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import com.rwmobi.kunigami.data.repository.DemoOctopusApiRepository
+import com.rwmobi.kunigami.domain.exceptions.HttpException
+import com.rwmobi.kunigami.domain.repository.FakeOctopusApiRepository
+import com.rwmobi.kunigami.domain.repository.FakeUserPreferencesRepository
+import com.rwmobi.kunigami.domain.usecase.account.SyncUserProfileUseCase
+import com.rwmobi.kunigami.domain.usecase.consumption.GenerateUsageInsightsUseCase
+import com.rwmobi.kunigami.domain.usecase.consumption.GetConsumptionAndCostUseCase
+import com.rwmobi.kunigami.domain.usecase.product.GetTariffUseCase
+import com.rwmobi.kunigami.test.samples.RateSampleData
+import com.rwmobi.kunigami.ui.destinations.usage.UsageScreenType
+import com.rwmobi.kunigami.ui.model.ScreenSizeInfo
+import com.rwmobi.kunigami.ui.model.SpecialErrorScreen
+import com.rwmobi.kunigami.ui.model.StubStringResourceProvider
+import com.rwmobi.kunigami.ui.model.consumption.ConsumptionPresentationStyle
+import com.rwmobi.kunigami.ui.previewsampledata.AccountSamples
+import com.rwmobi.kunigami.ui.previewsampledata.TariffSamples
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.datetime.Clock
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+import kotlin.time.Duration
+
+@OptIn(ExperimentalCoroutinesApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
+class UsageViewModelTest {
+
+    private lateinit var usageViewModel: UsageViewModel
+    private lateinit var octopusApiRepository: FakeOctopusApiRepository
+    private lateinit var userPreferencesRepository: FakeUserPreferencesRepository
+
+    @BeforeTest
+    fun setupViewModel() {
+        val dispatcher = UnconfinedTestDispatcher()
+        Dispatchers.setMain(dispatcher)
+        octopusApiRepository = FakeOctopusApiRepository()
+        userPreferencesRepository = FakeUserPreferencesRepository()
+
+        usageViewModel = UsageViewModel(
+            syncUserProfileUseCase = SyncUserProfileUseCase(
+                userPreferencesRepository = userPreferencesRepository,
+                octopusApiRepository = octopusApiRepository,
+                dispatcher = dispatcher,
+            ),
+            getTariffUseCase = GetTariffUseCase(
+                octopusApiRepository = octopusApiRepository,
+                dispatcher = dispatcher,
+            ),
+            getConsumptionAndCostUseCase = GetConsumptionAndCostUseCase(
+                userPreferencesRepository = userPreferencesRepository,
+                octopusApiRepository = octopusApiRepository,
+                demoOctopusApiRepository = DemoOctopusApiRepository(),
+                dispatcher = dispatcher,
+            ),
+            generateUsageInsightsUseCase = GenerateUsageInsightsUseCase(),
+            stringResourceProvider = StubStringResourceProvider(),
+            dispatcher = dispatcher,
+        )
+
+        usageViewModel.notifyScreenSizeChanged(
+            screenSizeInfo = ScreenSizeInfo(
+                heightPx = 1280,
+                widthPx = 1024,
+                heightDp = 1280.dp,
+                widthDp = 1024.dp,
+            ),
+            windowSizeClass = WindowSizeClass.calculateFromSize(
+                size = Size(
+                    width = 1280f,
+                    height = 1024f,
+                ),
+                density = Density(density = 1.0f),
+            ),
+        )
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    private fun setupDemoMode() {
+        userPreferencesRepository.demoMode = true
+        octopusApiRepository.setSimpleProductTariffResponse = Result.success(TariffSamples.agileFlex221125)
+        octopusApiRepository.setStandardUnitRatesResponse = Result.success(RateSampleData.rates)
+    }
+
+    private fun setupLoggedInMode() {
+        val accountSample = AccountSamples.account928
+        userPreferencesRepository.demoMode = false
+        userPreferencesRepository.apiKey = "sample-api-key"
+        userPreferencesRepository.accountNumber = accountSample.accountNumber
+        userPreferencesRepository.mpan = accountSample.electricityMeterPoints[0].mpan
+        userPreferencesRepository.meterSerialNumber = accountSample.getDefaultMeterSerialNumber()
+        octopusApiRepository.setAccountResponse = Result.success(accountSample)
+        octopusApiRepository.setSimpleProductTariffResponse = Result.success(TariffSamples.agileFlex221125)
+        octopusApiRepository.setStandardUnitRatesResponse = Result.success(RateSampleData.rates)
+        octopusApiRepository.setConsumptionResponse = Result.success(ConsumptionSampleData.randomSample)
+    }
+
+    @Test
+    fun `initialLoad should load user profile and consumption data successfully under demo mode`() = runTest {
+        setupDemoMode()
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertTrue(uiState.isDemoMode!!)
+        assertTrue(uiState.consumptionGroupedCells.isNotEmpty())
+        assertTrue(uiState.errorMessages.isEmpty())
+        assertNotNull(uiState.userProfile)
+        assertNotNull(uiState.barChartData)
+        assertNotNull(uiState.consumptionQueryFilter)
+        assertNotNull(uiState.insights)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertEquals(uiState.tariff, TariffSamples.agileFlex221125)
+    }
+
+    @Test
+    fun `initialLoad should load user profile and consumption data successfully`() = runTest {
+        setupLoggedInMode()
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertFalse(uiState.isDemoMode!!)
+        assertTrue(uiState.consumptionGroupedCells.isNotEmpty())
+        assertTrue(uiState.errorMessages.isEmpty())
+        assertNotNull(uiState.userProfile)
+        assertNotNull(uiState.barChartData)
+        assertNotNull(uiState.consumptionQueryFilter)
+        assertNotNull(uiState.insights)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertEquals(uiState.tariff, TariffSamples.agileFlex221125)
+    }
+
+    @Test
+    fun `initialLoad should return error message if failed to load consumption data`() = runTest {
+        setupLoggedInMode()
+        octopusApiRepository.setConsumptionResponse = Result.failure(RuntimeException("test exception"))
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertNotNull(uiState.errorMessages)
+    }
+
+    @Test
+    fun `initialLoad should return error message if failed to load standard unit rates`() = runTest {
+        setupLoggedInMode()
+        octopusApiRepository.setStandardUnitRatesResponse = Result.failure(RuntimeException("test exception"))
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertNotNull(uiState.errorMessages)
+    }
+
+    @Test
+    fun `initialLoad should handle HttpException`() = runTest {
+        setupLoggedInMode()
+        octopusApiRepository.setConsumptionResponse = Result.failure(HttpException(httpStatusCode = 403))
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Error(SpecialErrorScreen.HttpError(statusCode = 403)), uiState.requestedScreenType)
+    }
+
+    @Test
+    fun `initialLoad should handle UnresolvedAddressException`() = runTest {
+        setupLoggedInMode()
+        octopusApiRepository.setConsumptionResponse = Result.failure(UnresolvedAddressException())
+
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Error(SpecialErrorScreen.NetworkError), uiState.requestedScreenType)
+    }
+
+    @Test
+    fun `onSwitchPresentationStyle should switch the presentation style`() = runTest {
+        setupDemoMode()
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        usageViewModel.onSwitchPresentationStyle(
+            consumptionQueryFilter = usageViewModel.uiState.value.consumptionQueryFilter!!,
+            presentationStyle = ConsumptionPresentationStyle.MONTH_WEEKS,
+        )
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertEquals(ConsumptionPresentationStyle.MONTH_WEEKS, uiState.consumptionQueryFilter?.presentationStyle)
+    }
+
+    @Test
+    fun `onPreviousTimeFrame should navigate to the previous time frame`() = runTest {
+        setupDemoMode()
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val currentFilter = usageViewModel.uiState.value.consumptionQueryFilter!!
+        usageViewModel.onPreviousTimeFrame(consumptionQueryFilter = currentFilter)
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertTrue(uiState.consumptionQueryFilter?.requestedPeriod?.start!! < currentFilter.requestedPeriod.start)
+        assertTrue(uiState.errorMessages.isEmpty())
+    }
+
+    @Test
+    fun `onPreviousTimeFrame should return error message if failed to navigate`() = runTest {
+        setupLoggedInMode()
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val currentFilter = usageViewModel.uiState.value.consumptionQueryFilter!!.copy(
+            referencePoint = AccountSamples.account928.movedInAt!!,
+        )
+        usageViewModel.onPreviousTimeFrame(consumptionQueryFilter = currentFilter)
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertNotNull(uiState.errorMessages)
+    }
+
+    @Test
+    fun `onNextTimeFrame should navigate to the next time frame`() = runTest {
+        setupDemoMode()
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val currentFilter = usageViewModel.uiState.value.consumptionQueryFilter!!
+        usageViewModel.onNextTimeFrame(consumptionQueryFilter = currentFilter)
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertTrue(uiState.consumptionQueryFilter?.requestedPeriod?.start!! > currentFilter.requestedPeriod.start)
+        assertTrue(uiState.errorMessages.isEmpty())
+    }
+
+    @Test
+    fun `onNextTimeFrame should return error message if failed to navigate`() = runTest {
+        setupLoggedInMode()
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val currentFilter = usageViewModel.uiState.value.consumptionQueryFilter!!.copy(
+            referencePoint = Clock.System.now() + Duration.parse("2d"),
+        )
+        usageViewModel.onNextTimeFrame(consumptionQueryFilter = currentFilter)
+        advanceUntilIdle()
+
+        val uiState = usageViewModel.uiState.value
+        assertFalse(uiState.isLoading)
+        assertEquals(UsageScreenType.Chart, uiState.requestedScreenType)
+        assertNotNull(uiState.errorMessages)
+    }
+
+    @Test
+    fun `notifyScreenSizeChanged should update the UI state based on screen size`() = runTest {
+        usageViewModel.notifyScreenSizeChanged(
+            screenSizeInfo = ScreenSizeInfo(
+                heightPx = 720,
+                widthPx = 1280,
+                heightDp = 360.dp,
+                widthDp = 640.dp,
+            ),
+            windowSizeClass = WindowSizeClass.calculateFromSize(
+                size = Size(
+                    width = 640f,
+                    height = 360f,
+                ),
+                density = Density(density = 2.0f),
+            ),
+        )
+        val uiState = usageViewModel.uiState.value
+        assertEquals(WindowWidthSizeClass.Compact, uiState.requestedAdaptiveLayout)
+    }
+
+    @Test
+    fun `errorShown should remove the error message with the given ID`() = runTest {
+        setupLoggedInMode()
+        octopusApiRepository.setAccountResponse = Result.failure(RuntimeException("test exception"))
+        usageViewModel.initialLoad()
+        advanceUntilIdle()
+
+        val firstErrorMessageId = usageViewModel.uiState.value.errorMessages.first().id
+        usageViewModel.errorShown(errorId = firstErrorMessageId)
+
+        val uiState = usageViewModel.uiState.value
+        assertNull(uiState.errorMessages.firstOrNull { it.id == firstErrorMessageId })
+    }
+
+    @Test
+    fun `requestScrollToTop should update requestScrollToTop in UI state`() = runTest {
+        usageViewModel.requestScrollToTop(true)
+        val uiState = usageViewModel.uiState.value
+        assertTrue(uiState.requestScrollToTop)
+    }
+}
