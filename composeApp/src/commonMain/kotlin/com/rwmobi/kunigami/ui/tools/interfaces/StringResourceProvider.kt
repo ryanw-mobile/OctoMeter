b7caa14fc818fc01da233175011068309f28ca5c
Ryan W <ryan@rwmobi.com>
2024-09-07 19:35:05 +0100

    [docs] Copyright: update copyright text

--- composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/tools/interfaces/StringResourceProvider.kt
+++ composeApp/src/commonMain/kotlin/com/rwmobi/kunigami/ui/tools/interfaces/StringResourceProvider.kt
@@ -1,8 +1,16 @@
 /*
- * Copyright (c) 2024. Ryan Wong
- * https://github.com/ryanw-mobile
- * Sponsored by RW MobiMedia UK Limited
+ * Copyright (c) 2024. RW MobiMedia UK Limited
  *
+ * Contributions made by other developers remain the property of their respective authors but are licensed
+ * to RW MobiMedia UK Limited and others under the same licence terms as the main project, as outlined in
+ * the LICENSE file.
+ *
+ * RW MobiMedia UK Limited reserves the exclusive right to distribute this application on app stores.
+ * Reuse of this source code, with or without modifications, requires proper attribution to
+ * RW MobiMedia UK Limited.  Commercial distribution of this code or its derivatives without prior written
+ * permission from RW MobiMedia UK Limited is prohibited.
+ *
+ * Please refer to the LICENSE file for the full terms and conditions.
  */
 // In the :presentation module under ui/model package
 package com.rwmobi.kunigami.ui.tools.interfaces
