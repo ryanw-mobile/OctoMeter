3116db1358a6095d1cadd4b06dd3d86940e5af05
Ryan W <ryan@rwmobi.com>
2025-06-28 23:14:05 +0100

    [fix] App: fix to remove duplicated Android koin loggers

--- composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/KunigamiApplication.kt
+++ composeApp/src/androidMain/kotlin/com/rwmobi/kunigami/KunigamiApplication.kt
@@ -27,15 +27,12 @@ import com.rwmobi.kunigami.di.repositoryModule
 import com.rwmobi.kunigami.di.useCaseModule
 import com.rwmobi.kunigami.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.core.logger.Level
 
 class KunigamiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger(Level.ERROR)
             androidContext(this@KunigamiApplication)
             logger(
                 KermitKoinLogger(Logger.withTag("koin")),
